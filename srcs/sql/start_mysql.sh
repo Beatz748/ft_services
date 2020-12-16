ee34cfde291180e8b976aa443a4182a1a26499e0
UGF3ZWwgTW9za2Fsw7xr TW9za2Fsw7xr <kshantel@ga-u1.msk.21-school.ru>
2020-12-16 22:06:37 +0300

    loler

--- srcs/sql/start_mysql.sh
+++ srcs/sql/start_mysql.sh
@@ -2,3 +2,7 @@
 
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
 /usr/bin/mysqld --console --init_file=/etc/mysql/config.sql
+echo "CREATE DATABASE wordpress;" | mysql
+echo "GRANT ALL ON wordpress.* TO 'mysql'@'%' WITH GRANT OPTION;" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
+mysql -u mysql wordpress < etc/mysql/wordpress.sql

5e2adf59035f40dc8581b76b5499f677840e698d
UGF3ZWwgTW9za2Fsw7xr TW9za2Fsw7xr <kshantel@ga-u1.msk.21-school.ru>
2020-12-23 22:03:39 +0300

    gggggg

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -87,6 +87,6 @@ define('WP_DEBUG', false);
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
-
+	define( 'CONCATENATE_SCRIPTS', false );
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
